041879b2f0f912d90655dc7043bbd03befa65d62
igor bruno <igordf2502@gmail.com>
2026-05-06 20:28:02 -0300

    correção footer profissionalmente
    Signed-off-by: igor bruno <igordf2502@gmail.com>

--- praticas/pratica04/chronos-pomodoro/src/components/Footer/index.tsx
+++ praticas/pratica04/chronos-pomodoro/src/components/Footer/index.tsx
@@ -4,13 +4,25 @@ import { RouterLink } from '../RouterLink';
 export function Footer() {
     return (
         <footer className={styles.footer}>
-            <RouterLink href='/about-pomodoro/'>
-                Entenda como funciona a técnica pomodoro feita por alunos
-            </RouterLink>
+            <div className={styles.footerContent}>
+                <p className={styles.description}>
+                    Organize seu tempo, mantenha o foco e acompanhe sua produtividade.
+                </p>
 
-            <RouterLink href='/'>
-                Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com carinho
-            </RouterLink>
+                <nav className={styles.links}>
+                    <RouterLink href='/about-pomodoro/'>
+                        Entenda a técnica Pomodoro
+                    </RouterLink>
+
+                    <RouterLink href='/'>
+                        Chronos Pomodoro
+                    </RouterLink>
+                </nav>
+
+                <p className={styles.copy}>
+                    &copy; {new Date().getFullYear()} Chronos Pomodoro. Todos os direitos reservados.
+                </p>
+            </div>
         </footer>
     );
 }
